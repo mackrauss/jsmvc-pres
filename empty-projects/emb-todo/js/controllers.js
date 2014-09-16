9b7097f8c3ca649c281077bb1e201d125ab84440
Armin Krauss <armin.krauss@utoronto.ca>
2014-09-16 13:01:24 -0400

    Ember example finished

--- empty-projects/emb-todo/js/controllers.js
+++ empty-projects/emb-todo/js/controllers.js
@@ -1,13 +1,32 @@
 // individual Object
 Todos.TodoController = Ember.ObjectController.extend({
   actions: {
-
+    removeTodo: function() {
+      // Ember knows which record to get
+      var todo = this.get('model');
+      todo.deleteRecord();
+      todo.save();
+    }
   }
 });
 
 // Collection of objects
 Todos.TodosController = Ember.ArrayController.extend({
   actions: {
-
+    createNewTodo: function() {
+      var newVal = this.get('newTodo');
+      var todo = this.store.createRecord('todo', {
+        val: newVal,
+        completed: false
+      });
+      this.set('newTodo', '');
+      todo.save();
+    },
+    clearCompleted: function() {
+      // filterBy finds all todo elements with completed being true
+      var completed = this.filterBy("completed", true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save')
+    }
   }
 });
